7c715fa7ce6a9928668966b49df3aad24b8b5bb8
Vicente <ivcente@gmail.com>
2025-10-05 02:34:42 -0300

    DanzmannMetaprogrammingUtils v2.0 | Add DanzmannToRawPtr

---
+++ Source/DanzmannMetaprogrammingUtils/Public/DanzmannToRawPtr.h
@@ -0,0 +1,32 @@
+// Copyright (C) 2025 Dancing Man Games. All Rights Reserved.
+
+#pragma once
+
+#include "DanzmannIsRawPtrDerivedFrom.h"
+#include "DanzmannIsTSoftClassPtrDerivedFrom.h"
+#include "DanzmannIsTSoftObjectPtrDerivedFrom.h"
+
+namespace Danzmann
+{
+	/**
+	 * Try to extract and return a raw pointer from T during compile-time. T is expected to already
+	 * be a raw pointer (which would just return itself) or a TSoftObjectPtr/TSoftClassPtr
+	 * @tparam T Parameter to extract raw pointer.
+	 */
+	template<typename T>
+	constexpr UObject* ToRawPointer(const T Asset)
+	{
+		if constexpr (Danzmann::IsRawPtrDerivedFrom<T, UObject>())
+		{
+			return Asset;
+		}
+		else if constexpr (Danzmann::IsTSoftObjectPtrDerivedFrom<T, UObject>() || Danzmann::IsTSoftClassPtrDerivedFrom<T, UObject>())
+		{
+			return Asset.Get();
+		}
+		else
+		{
+			return nullptr;
+		}
+	}
+};
